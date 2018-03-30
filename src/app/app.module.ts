7ec2bed1f5c9702e7eca3bf5a0391565724a8625
Vu Truong <truong.vu2106@gmail.com>
2018-03-31 00:57:25 +0700

    rename page to pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component'
-import { LoginComponent } from './page/login/login.component'
+import { LoginComponent } from './pages/login/login.component'
 
 
 @NgModule({
