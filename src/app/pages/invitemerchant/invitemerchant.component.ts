64ed6e187f408b2073865cda941e68520e49c374
Vu Truong <truong.vu2106@gmail.com>
2018-04-05 19:37:36 +0700

    update invite merchant

--- src/app/pages/invitemerchant/invitemerchant.component.ts
+++ src/app/pages/invitemerchant/invitemerchant.component.ts
@@ -1,26 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormControl} from '@angular/forms';
+import { DataService } from '../../services/data.service';
+
+
 @Component({
   selector: 'app-invitemerchant',
   templateUrl: './invitemerchant.component.html',
-  styleUrls: ['./invitemerchant.component.scss']
+  styleUrls: ['./invitemerchant.component.scss'],
+  providers:[DataService]
 })
 export class InviteMerchantComponent implements OnInit {
 
-  a=true;
+  // text MS error load json
+  errorMessage: string;
+  @ViewChild ('autocompalete') autocompalete;
+
   //search merchant name
   searchFrmControl: FormControl = new FormControl();
-  
-  merchantNameList = [];
+  //list merchant finded
+  merchantFinded : any[] = [];
 
-  constructor() {
-    console.log(this.searchFrmControl)
-   }
+  merchantNameList: any = [];
+
+  merchantSelected: any = {};
+
+  // changedName: any =[];
+
+  constructor(private dataService: DataService) {
+  }
 
   ngOnInit() {
   }
 
-  searchMerchantName(searchFrmControl){
+  searchMerchantName(searchFrmControl, event) {
     console.log(searchFrmControl);
+    event.preventDefault();
+    event.stopPropagation();
+    if(searchFrmControl.value!==""){
+      this.dataService.getData('assets/data/merchantinvite.json').subscribe(
+        data => {
+          //Match name in data equal to input
+          this.merchantFinded = data.merchant.filter((item) => {
+            return item.merchantName.toLowerCase().indexOf(searchFrmControl.value.toLowerCase()) > -1 ;
+          }) || {};
+          // get merchantNameList
+          this.merchantNameList = this.merchantFinded.map(
+              (item, index)=>{
+                var a = [];
+                item.merchantNameChange = item.merchantName.toLowerCase().split(searchFrmControl.value.toLowerCase());
+                // for( var i of this.changedName){
+                //   if(item.merchantName.search(i)>-1){
+                //     a.push[i];
+                //     item.merchantName.substr(i.length,searchFrmControl.value.length);
+                //   }
+                // }
+                //this.searchKey = this.searchKey.substr(item.merchantName[0].length,searchFrmControl.value.length);
+                return item;
+            }
+          )||{};
+          console.log(this.merchantNameList);
+        },
+        error => this.errorMessage = <any>error
+      );
+    }else{
+      this.merchantNameList=[];
+    }
+  }
+  doSearch() {
+    this.merchantNameList = [];
+  }
+  selectMerchant(item) {
+    this.merchantSelected = item;
+    this.addUser();
+  }
+  addUser() {
+    let user = {
+      "firstName":"",
+      "lastName":"",
+      "Email":"",
+      "MobileNo":""
+    }
+    this.merchantSelected.userInvite.push(user);
   }
 }
