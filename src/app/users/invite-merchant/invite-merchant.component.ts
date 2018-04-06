dac9abfd1ee5f124c8a1594fca59ee6735113968
Vu Truong <truong.vu2106@gmail.com>
2018-04-06 13:06:39 +0700

    complete invite user

--- src/app/users/invite-merchant/invite-merchant.component.ts
+++ src/app/users/invite-merchant/invite-merchant.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import { DataService } from '../../core/services/data.service';
+import { Store } from "@ngrx/store";
+import { Go } from "../../actions/router";
+// reducers
+import {
+  State
+} from "../../reducers";
 import * as _ from 'lodash';
 
 @Component({
@@ -24,50 +30,84 @@ export class InviteMerchantComponent implements OnInit {
 
   merchantSelected: any = {};
   userInviting: any = {};
-  // changedName: any =[];
+  //show searched value
+  isShowResult:boolean = false;
+  dataMaster:any;
+  isShowPannel:boolean;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService, private store: Store<State>) {
   }
 
   ngOnInit() {
   }
 
   searchMerchantName(searchFrmControl, event) {
-    console.log(searchFrmControl);
     event.preventDefault();
     event.stopPropagation();
-    if(searchFrmControl.value!==""){
+    if(event.which <= 90 && event.which >= 48){// letter and number
+      this.loadData(searchFrmControl,undefined);
+      this.isShowResult=false;
+    }else{
+      if(event.which === 8){
+        if( searchFrmControl.value==""){// backspace to empty value
+          this.merchantNameList=[];
+          this.isShowResult=false;
+        }else{
+          this.loadData(searchFrmControl,undefined);
+          this.isShowResult=false;
+        }
+      }
+      if(event.which === 13 && searchFrmControl.value!=""){// enter when value not null
+        this.showResult(searchFrmControl);
+        this.merchantNameList=[];
+      }
+    }
+  }
+  loadData(searchFrmControl,callback){
       this.dataService.getData('assets/data/merchantinvite.json').subscribe(
         data => {
           //Match name in data equal to input
-          this.merchantFinded = data.merchant.filter((item) => {
+          this.dataMaster = data;
+          console.log("loading..")
+          console.log(this.merchantFinded);
+        },
+        error => this.errorMessage = <any>error,
+        ()=> {
+          console.log("load success")
+          console.log(this.merchantFinded);
+          this.merchantFinded = this.dataMaster.merchant.filter((item) => {
             return item.merchantName.toLowerCase().indexOf(searchFrmControl.value.toLowerCase()) > -1 ;
           }) || {};
-          // get merchantNameList
-          this.merchantNameList = this.merchantFinded.map(
-              (item, index)=>{
-                var a = [];
-                item.merchantNameChange = item.merchantName.toLowerCase().split(searchFrmControl.value.toLowerCase());
-                // for( var i of this.changedName){
-                //   if(item.merchantName.search(i)>-1){
-                //     a.push[i];
-                //     item.merchantName.substr(i.length,searchFrmControl.value.length);
-                //   }
-                // }
-                //this.searchKey = this.searchKey.substr(item.merchantName[0].length,searchFrmControl.value.length);
-                return item;
-            }
-          )||{};
-          console.log(this.merchantNameList);
-        },
-        error => this.errorMessage = <any>error
+          this.addAttrMerchantList(searchFrmControl)
+          if(callback){
+            callback()
+          }
+        }
       );
-    }else{
-      this.merchantNameList=[];
-    }
+      
   }
-  doSearch() {
-    this.merchantNameList = [];
+  addAttrMerchantList(searchFrmControl){
+    console.log("addAttrMerchantList actived");
+    this.merchantNameList=[];
+    this.merchantNameList = this.merchantFinded.map(
+      (item, index)=>{
+        var a = [];
+        item.merchantNameChange = item.merchantName.toLowerCase().split(searchFrmControl.value.toLowerCase());
+        return item;
+      }
+    )||{};
+    console.log(this.merchantNameList);
+    return true
+  }
+  showResult(searchFrmControl){
+    console.log("showResult actived");
+    console.log(searchFrmControl);
+    this.loadData(searchFrmControl,()=>{
+      this.merchantNameList=[];
+      this.isShowResult=true;
+      console.log(this.merchantNameList);
+    });
+    
   }
   selectMerchant(item) {
     this.merchantSelected = item;
@@ -84,5 +124,12 @@ export class InviteMerchantComponent implements OnInit {
   }
   removeUser(user) {
     _.remove(this.merchantSelected.userInvite, user);
+  }
+  sendInvite() {
+    if(this.merchantSelected && this.merchantSelected.userInvite && this.merchantSelected.userInvite.length) {
+      this.store.dispatch(new Go({
+        path: ["/users/manage-merchant"]
+      }));
+    }
   }
 }
